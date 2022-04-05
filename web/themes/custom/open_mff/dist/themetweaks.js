c9a7e32cf34d62474f1b01b84e8884b41e2b1e8a
Patrick Mandigo <plato1123@gmail.com>
2022-04-05 03:05:57 -0700

    mff-105: Additional style tweaks for directory.

---
+++ web/themes/custom/open_mff/dist/themetweaks.js
@@ -0,0 +1,24 @@
+// (function ($, Drupal) {
+//   Drupal.behaviors.someAbitraryKey = {
+//     attach: function (context, settings) {
+//       // Do something when the page loads.
+//       alert("is our theme tweaking yeeeet?");
+//     }
+//   };
+// }(jQuery, Drupal));
+
+// (function ($, Drupal) {
+//   $(window).on('load', function() {
+//       Drupal.behaviors.myBehavior = {
+//           attach: function (context, settings) {
+//                  $(context)
+//                 .find('.funcolumns')
+//                  .once('.funcolumns')
+//                  .each(function () {
+//                   $(this).addClass('superfunforus');
+//                   alert('derp');
+//                });
+//           }
+//       };
+//   });
+// })(jQuery, Drupal);
